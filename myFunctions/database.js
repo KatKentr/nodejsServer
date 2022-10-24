e793613ac7ea8e5cebf3175c4a71ffa0c978ab52
KatKentr <kkentroti@gmail.com>
2022-10-24 18:43:46 +0300

    connection pool method

--- myFunctions/database.js
+++ myFunctions/database.js
@@ -2,16 +2,19 @@
 
 import mysql from 'mysql';
 
-const conn = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 498,
   host: 'localhost', // Replace with your host name
   user: 'root',      // Replace with your database username
   password: '1234_Ken',      // Replace with your database password
   database: 'fruits' // // Replace with your database Name
+  
 });
-
+/*
 conn.connect(function(err) {
   if (err) throw err;
   console.log('Database is connected successfully !');
 });
+*/
 
-export { conn };
+export { pool };
--- myFunctions/fetchData_route.js
+++ myFunctions/fetchData_route.js
@@ -3,7 +3,8 @@
 //import the epress application object and the connection to the database
 import express from 'express';
 //import {conn} from './database.js'
-import mysql from 'mysql';
+//import mysql from 'mysql';
+import {pool} from './database.js';
 
 //get the Router object
 const router=express.Router();
@@ -12,14 +13,17 @@ const router=express.Router();
 router.get('/fetchData', function(req, res) {
     let sql='SELECT * FROM dummyData';
     
+    
+    /*
     const conn = mysql.createConnection({
       host: 'localhost', // Replace with your host name
       user: 'root',      // Replace with your database username
       password: '1234_Ken',      // Replace with your database password
       database: 'fruits' // // Replace with your database Name
    });
+   */
    
-    conn.query(sql, function (err, rows) {
+    pool.query(sql, function (err, rows) {
      
      //conn.end();
      
@@ -44,10 +48,12 @@ router.get('/fetchData', function(req, res) {
   //close connection
   //conn.end();
    // Close the connection.
+   /*
     conn.end((err => {
         if(err) throw err;
         //console.log("Connection closed.");
     }));
+    */
 });
 
 
