e838b510c1af9a008428060d894034a4bf51cf9d
KatKentr <kkentroti@gmail.com>
2022-10-25 20:42:00 +0300

    cluster module implemented succesfully

--- server.js
+++ server.js
@@ -2,12 +2,45 @@
 //import all the routes
 import {router as fibonacci} from './myFunctions/fibonacci_route.js'
 import {router as dbTest} from './myFunctions/fetchData_route.js'
-
 //treats as a commonJS script
 //const express = require("express");
 import express from 'express';
-const app = express();
+
+import cluster from 'node:cluster';
+import { cpus } from 'node:os';
+import process from 'node:process';
+
+const numCPUs = cpus().length;
+
+
+
 const port = 4000;
+
+
+if (cluster.isPrimary) {
+  //console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+  
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+  
+} else {
+
+  const app = express();
+  //console.log(`Worker ${process.pid} started`);
+
+  
 
 /*
 app.get("/", (req, res) => {
@@ -16,9 +49,11 @@ app.get("/", (req, res) => {
 });
 */
 
-app.use("/fibonacci", fibonacci);
-app.use("/dbTest",dbTest);
+ app.use("/fibonacci", fibonacci);
+ app.use("/dbTest",dbTest);
 
-app.listen(port, () => {
+ app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
-});
+ });
+ 
+ }
